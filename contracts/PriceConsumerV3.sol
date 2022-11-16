ed2205998fba87c5b5e05b74570aab3b9f6c6782
kundan mishra <mishrakundan073@gmail.com>
2022-11-16 12:04:31 +0530

    Update PriceConsumerV3.sol

--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -6,14 +6,14 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract PriceConsumerV3 {
   AggregatorV3Interface internal priceFeed;
 
-  /**
-   * Network: Rinkeby
-   * Aggregator: ETH/USD
-   * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-   */
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
   constructor() {
     priceFeed = AggregatorV3Interface(
-      0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
     );
   }
 
